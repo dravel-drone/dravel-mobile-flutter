f8f48323060e3ad3acc2423f26036d41a9fd029f
K1A2 <jckim0307@gmail.com>
2024-07-23 21:18:53 +0900

    DRAV-2 design: 커스텀 appbar 추가

---
+++ lib/widgets/appbar/appbar_main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  CustomAppbar({
+    required this.title,
+    this.leading,
+    this.actions = const [],
+    this.backgroundColor,
+    this.surfaceTintColor,
+    this.textColor
+  });
+
+  String title;
+
+  Widget? leading;
+  List<Widget> actions;
+
+  Color? backgroundColor;
+  Color? surfaceTintColor;
+  Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    textColor ??= Colors.white;
+
+    return AppBar(
+      backgroundColor: backgroundColor,
+      surfaceTintColor: surfaceTintColor,
+      centerTitle: true,
+      automaticallyImplyLeading: false,
+      title: Text(
+        title,
+        style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+            color: textColor!
+        ),
+      ),
+      leading: leading,
+      actions: actions,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
