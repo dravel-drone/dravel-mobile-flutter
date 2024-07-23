941083248aaed182922b19d5e08eab5b5d9fac22
K1A2 <jckim0307@gmail.com>
2024-07-23 21:22:42 +0900

    DRAV-2 design: 로그인 앱바 추가

--- lib/pages/page_login.dart
+++ lib/pages/page_login.dart
@@ -1,4 +1,6 @@
+import 'package:dravel/widgets/appbar/appbar_main.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -9,6 +11,16 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: CustomAppbar(
+        title: "로그인",
+        textColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_outlined),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+      ),
       body: SafeArea(
         child: Text("login"),
       ),
