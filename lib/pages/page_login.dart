43ae71da574b290ae55cb9a44df41a69ab39b5f0
K1A2 <jckim0307@gmail.com>
2024-07-23 22:16:22 +0900

    DRAV-2 design: 아이디, 비밀번호 텍스트 입력창 추가

--- lib/pages/page_login.dart
+++ lib/pages/page_login.dart
@@ -1,4 +1,6 @@
 import 'package:dravel/widgets/appbar/appbar_main.dart';
+import 'package:dravel/widgets/textField/textfield_main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,6 +10,45 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  Widget _textFiledEmail() => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text('이메일'),
+      SizedBox(height: 8,),
+      MainTextField(
+          prefixIcon: Icon(
+            Icons.mail_rounded,
+            color: Colors.black45,
+          )
+      )
+    ],
+  );
+
+  Widget _textFiledPassword() => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text('비밀번호'),
+      SizedBox(height: 8,),
+      MainTextField(
+          prefixIcon: Icon(
+            Icons.lock_rounded,
+            color: Colors.black45,
+          )
+      )
+    ],
+  );
+
+  Widget _createTextInputSection() {
+    return Column(
+      children: [
+        _textFiledEmail(),
+        SizedBox(height: 24,),
+        _textFiledPassword()
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +63,15 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
       body: SafeArea(
-        child: Text("login"),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(24, 35, 24, 24),
+          width: double.infinity,
+          child: Column(
+            children: [
+              _createTextInputSection()
+            ],
+          ),
+        )
       ),
     );
   }
