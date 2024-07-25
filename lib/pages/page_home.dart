c564184a4c55324179808b73f5f68b7f39359a9d
K1A2 <jckim0307@gmail.com>
2024-07-25 21:28:06 +0900

    DRAV-98 design: 홈화면 제목 추가

--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -4,7 +4,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Text('home'),
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.fromLTRB(24, 24, 24, 0),
+        child: Column(
+          children: [
+            Text(
+              '여행지 탐색',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
