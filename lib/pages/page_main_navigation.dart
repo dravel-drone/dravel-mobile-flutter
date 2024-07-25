02febb75e6376c6c1a54a9b70aa07ca554526328
K1A2 <jckim0307@gmail.com>
2024-07-25 21:13:28 +0900

    DRAV-98 design: 바텀바 둥근 테두리 추가

--- lib/pages/page_main_navigation.dart
+++ lib/pages/page_main_navigation.dart
@@ -15,38 +15,49 @@ class _MainNavigationPageState extends State<MainNavigationPage> {
       body: SafeArea(
         child: Text('메인페이지'),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        currentIndex: _selectedPageIdx,
-        unselectedItemColor: Colors.black,
-        selectedItemColor: Colors.blue,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.map_outlined
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(24),
+          topRight: Radius.circular(24)
+        ),
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: _selectedPageIdx,
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.blue,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                    Icons.map_outlined
+                ),
+                label: '지도'
             ),
-            label: '지도'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_outlined
+            BottomNavigationBarItem(
+                icon: Icon(
+                    Icons.home_outlined
+                ),
+                label: '홈'
             ),
-            label: '홈'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.favorite_border_outlined
+            BottomNavigationBarItem(
+                icon: Icon(
+                    Icons.favorite_border_outlined
+                ),
+                label: '관심'
             ),
-            label: '관심'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.account_circle_outlined
+            BottomNavigationBarItem(
+                icon: Icon(
+                    Icons.account_circle_outlined
+                ),
+                label: '프로필'
             ),
-            label: '프로필'
-          ),
-        ],
+          ],
+          onTap: (value) {
+            setState(() {
+              _selectedPageIdx = value;
+            });
+          },
+        ),
       ),
     );
   }
