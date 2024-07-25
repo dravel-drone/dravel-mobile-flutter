05f52a3b5d0cd8e0305f2d049ca320a8b222046e
K1A2 <jckim0307@gmail.com>
2024-07-25 18:28:00 +0900

    DRAV-98 design: 메인페이지 파일 생성

---
+++ lib/pages/page_main_navigation.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainNavigationPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MainNavigationPageState();
+}
+
+class _MainNavigationPageState extends State<MainNavigationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
