29e3039fa6d57f0b1d40e9bf2dd4ac3651b77c23
K1A2 <jckim0307@gmail.com>
2024-07-25 21:04:59 +0900

    DRAV-98 design: 바텀바 추가

--- lib/pages/page_main_navigation.dart
+++ lib/pages/page_main_navigation.dart
@@ -6,8 +6,48 @@ class MainNavigationPage extends StatefulWidget {
 }
 
 class _MainNavigationPageState extends State<MainNavigationPage> {
+  int _selectedPageIdx = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFF1F1F5),
+      body: SafeArea(
+        child: Text('메인페이지'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedPageIdx,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.blue,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.map_outlined
+            ),
+            label: '지도'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined
+            ),
+            label: '홈'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite_border_outlined
+            ),
+            label: '관심'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle_outlined
+            ),
+            label: '프로필'
+          ),
+        ],
+      ),
+    );
   }
 }
