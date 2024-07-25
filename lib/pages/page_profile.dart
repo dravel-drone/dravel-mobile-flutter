db30310e5364b986f6bce49e4369ee89c71b36f0
K1A2 <jckim0307@gmail.com>
2024-07-25 22:42:10 +0900

    test: 로그인 페이지 이동 버튼 추가

--- lib/pages/page_profile.dart
+++ lib/pages/page_profile.dart
@@ -1,10 +1,16 @@
+import 'package:dravel/pages/account/page_login.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Text('profile'),
+      child: Column(
+        children: [
+          FilledButton(onPressed: () {Get.to(() => LoginPage());}, child: Text('login page'))
+        ],
+      ),
     );
   }
 }
