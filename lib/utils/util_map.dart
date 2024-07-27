d70d9cbd35b96ab0efb37f2e5a01c29ecc71fb07
K1A2 <jckim0307@gmail.com>
2024-07-27 16:26:05 +0900

    DRAV-29 feat: 단위 변환 함수 추가

---
+++ lib/utils/util_map.dart
@@ -0,0 +1,20 @@
+String formatDistance(int distanceInMeters) {
+  if (distanceInMeters < 1000) {
+    return '${distanceInMeters}m';
+  } else {
+    double distanceInKm = distanceInMeters / 1000;
+    if ((distanceInKm * 10) % 10 == 0) {
+      return '${distanceInKm.toInt()}km';
+    }
+    return '${distanceInKm.toStringAsFixed(1)}km';
+  }
+}
+
+String formatTime(int minute) {
+  String time = '';
+  if (minute ~/ 60 > 0) {
+    time += '${minute ~/ 60}h ';
+  }
+  time += '${minute % 60}m';
+  return time;
+}
