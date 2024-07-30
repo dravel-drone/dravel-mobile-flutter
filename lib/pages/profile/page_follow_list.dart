e306c8895969a1dab3c8bda6b89a0283581746e1
K1A2 <jckim0307@gmail.com>
2024-07-31 01:31:38 +0900

    DRAV-43 design: 상단바 추가

--- lib/pages/profile/page_follow_list.dart
+++ lib/pages/profile/page_follow_list.dart
@@ -1,4 +1,6 @@
+import 'package:dravel/widgets/appbar/appbar_main.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class FollowListPage extends StatefulWidget {
   static const bool FOLLOWING_MODE = false;
@@ -17,6 +19,20 @@ class FollowListPage extends StatefulWidget {
 class _FollowListPageState extends State<FollowListPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFF1F1F5),
+      appBar: CustomAppbar(
+        title: widget.mode == FollowListPage.FOLLOWER_MODE ?
+          '팔로워 목록' : '팔로잉 목록',
+        textColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_outlined),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
   }
 }
