b2c661ba84efe3174a00f27ff6057f57098d54b1
K1A2 <jckim0307@gmail.com>
2024-07-30 21:23:44 +0900

    DRAV-44 feat: 프로필 편집 페이지 이동 추가

---
+++ lib/pages/profile/page_profile_edit.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProfileEditPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ProfileEditPageState();
+}
+
+class _ProfileEditPageState extends State<ProfileEditPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
