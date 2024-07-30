34764e96e8a0f53eab35b03b2d6a29cea5370edc
K1A2 <jckim0307@gmail.com>
2024-07-30 22:38:27 +0900

    DRAV-44 design: 프로필 설정 버튼 추가

--- lib/pages/profile/page_profile_edit.dart
+++ lib/pages/profile/page_profile_edit.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dravel/widgets/appbar/appbar_main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -23,7 +24,46 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
         ),
         backgroundColor: Color(0xFFF1F1F5),
       ),
-      body: Container(),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(200),
+                    child: CachedNetworkImage(
+                      width: 120,
+                      height: 120,
+                      fit: BoxFit.cover,
+                      imageUrl: "https://images.unsplash.com/photo-1498141321056-776a06214e24?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    ),
+                  ),
+                  Material(
+                    color: Colors.transparent,
+                    child: Ink(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(200),
+                      ),
+                      width: 120,
+                      height: 120,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(200),
+                        onTap: () {
+                          debugPrint("test");
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
