68d1ac76c8c30da79c7810a316b6b36ea48e3be8
K1A2 <jckim0307@gmail.com>
2024-07-30 21:27:06 +0900

    DRAV-44 design: 프로필 편집 상단바 추가

--- lib/pages/profile/page_profile_edit.dart
+++ lib/pages/profile/page_profile_edit.dart
@@ -1,4 +1,6 @@
+import 'package:dravel/widgets/appbar/appbar_main.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfileEditPage extends StatefulWidget {
   @override
@@ -8,6 +10,20 @@ class ProfileEditPage extends StatefulWidget {
 class _ProfileEditPageState extends State<ProfileEditPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFF1F1F5),
+      appBar: CustomAppbar(
+        title: '프로필 편집',
+        textColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_outlined),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+        backgroundColor: Color(0xFFF1F1F5),
+      ),
+      body: Container(),
+    );
   }
 }
