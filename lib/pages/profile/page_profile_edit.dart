d1c3a5c3f8fa895f5e930122f15df9c08bc92391
K1A2 <jckim0307@gmail.com>
2024-07-30 22:46:43 +0900

    DRAV-44 design: 프로필 편집 버튼 추가

--- lib/pages/profile/page_profile_edit.dart
+++ lib/pages/profile/page_profile_edit.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dravel/utils/util_ui.dart';
 import 'package:dravel/widgets/appbar/appbar_main.dart';
+import 'package:dravel/widgets/button/button_main.dart';
 import 'package:dravel/widgets/textField/textfield_main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -10,6 +12,99 @@ class ProfileEditPage extends StatefulWidget {
 }
 
 class _ProfileEditPageState extends State<ProfileEditPage> {
+
+  Widget _createEditSection() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(200),
+                child: CachedNetworkImage(
+                  width: 120,
+                  height: 120,
+                  fit: BoxFit.cover,
+                  imageUrl: "https://images.unsplash.com/photo-1498141321056-776a06214e24?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                ),
+              ),
+              Material(
+                color: Colors.transparent,
+                child: Ink(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(200),
+                  ),
+                  width: 120,
+                  height: 120,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(200),
+                    onTap: () {
+                      debugPrint("test");
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 24,),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text('닉네임'),
+          ),
+          SizedBox(height: 8,),
+          MainTextField(
+            hintText: '닉네임',
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.black45,
+            ),
+            action: TextInputAction.done,
+            onEditingComplete: () {
+              debugPrint("next");
+              FocusScope.of(context).unfocus();
+            },
+          ),
+          SizedBox(height: 24,),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text('한줄소개'),
+          ),
+          SizedBox(height: 8,),
+          MainTextField(
+            hintText: '한줄소개',
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.black45,
+            ),
+            action: TextInputAction.done,
+            onEditingComplete: () {
+              debugPrint("next");
+              FocusScope.of(context).unfocus();
+            },
+          ),
+          SizedBox(height: 24,),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text('보유 드론 (선택)'),
+          ),
+          SizedBox(height: 8,),
+          MainTextField(
+            hintText: '드론 이름',
+            prefixIcon: Icon(
+              Icons.flight_outlined,
+              color: Colors.black45,
+            ),
+            action: TextInputAction.done,
+            onEditingComplete: () {
+              debugPrint("next");
+              FocusScope.of(context).unfocus();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,59 +120,26 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
         ),
         backgroundColor: Color(0xFFF1F1F5),
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
-        child: SingleChildScrollView(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: EdgeInsets.fromLTRB(24, 0, 24, getBottomPaddingWithHeight(context, 24)),
           child: Column(
             children: [
-              Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(200),
-                    child: CachedNetworkImage(
-                      width: 120,
-                      height: 120,
-                      fit: BoxFit.cover,
-                      imageUrl: "https://images.unsplash.com/photo-1498141321056-776a06214e24?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    ),
-                  ),
-                  Material(
-                    color: Colors.transparent,
-                    child: Ink(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(200),
-                      ),
-                      width: 120,
-                      height: 120,
-                      child: InkWell(
-                        borderRadius: BorderRadius.circular(200),
-                        onTap: () {
-                          debugPrint("test");
-                        },
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: 24,),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text('닉네임'),
+              Expanded(
+                child: _createEditSection(),
               ),
               SizedBox(height: 8,),
-              MainTextField(
-                hintText: '닉네임',
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: Colors.black45,
+              SizedBox(
+                height: 44,
+                width: double.infinity,
+                child: MainButton(
+                    onPressed: () {
+
+                    },
+                    childText: '저장'
                 ),
-                action: TextInputAction.done,
-                onEditingComplete: () {
-                  debugPrint("next");
-                  FocusScope.of(context).unfocus();
-                },
               )
             ],
           ),
