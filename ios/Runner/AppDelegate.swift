874189963351fbb66c80fa609b8f37363b8e0a1b
K1A2 <jckim0307@gmail.com>
2024-07-31 23:26:27 +0900

    DRAV-106 chore: ios kakaomap 패키지 초기화

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,30 @@
 import Flutter
 import UIKit
+import KakaoMapsSDK
+
+import Foundation
+
+extension Bundle {
+    var kakao_key: String? {
+        return infoDictionary?["KAKAO_KEY"] as? String
+    }
+}
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        let kakaoKey: String? = Bundle.main.kakao_key
+        if (kakaoKey != nil) {
+            debugPrint("kakao key: " + kakaoKey!)
+            SDKInitializer.InitSDK(appKey: kakaoKey!)
+        }
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
