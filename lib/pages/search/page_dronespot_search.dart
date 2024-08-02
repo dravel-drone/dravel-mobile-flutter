55e1c240a3cc5b13001c78171c37794f2fdef97f
K1A2 <jckim0307@gmail.com>
2024-08-02 22:01:03 +0900

    DRAV-49 feat: 검색 화면 이동 추가

---
+++ lib/pages/search/page_dronespot_search.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DroneSpotSearchPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _DroneSpotSearchPageState();
+}
+
+class _DroneSpotSearchPageState extends State<DroneSpotSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
