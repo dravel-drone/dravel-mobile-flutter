5942546e0388eeda5a1b73c528cd3f00e2ecf953
K1A2 <jckim0307@gmail.com>
2024-08-08 09:53:53 +0900

    DRAV-16 design: 상단바 추가

--- lib/pages/detail/page_dronespot_detail.dart
+++ lib/pages/detail/page_dronespot_detail.dart
@@ -1,4 +1,6 @@
+import 'package:dravel/widgets/appbar/appbar_main.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DroneSpotDetailPage extends StatefulWidget {
   @override
@@ -8,6 +10,19 @@ class DroneSpotDetailPage extends StatefulWidget {
 class _DroneSpotDetailPageState extends State<DroneSpotDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFF1F1F5),
+      appBar: CustomAppbar(
+        title: "장소이름",
+        textColor: Colors.black,
+        backgroundColor: Color(0xFFF1F1F5),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_outlined),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+      ),
+    );
   }
 }
