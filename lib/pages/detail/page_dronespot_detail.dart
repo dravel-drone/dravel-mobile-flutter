4075a5271cfb26ca1f005916cdeda2e325751733
K1A2 <jckim0307@gmail.com>
2024-08-10 16:49:22 +0900

    DRAV-16 design: 드론스팟 리뷰 카드 추가

--- lib/pages/detail/page_dronespot_detail.dart
+++ lib/pages/detail/page_dronespot_detail.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dravel/utils/util_ui.dart';
 import 'package:dravel/widgets/appbar/appbar_main.dart';
+import 'package:dravel/widgets/list/list_item_review.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,6 +11,38 @@ class DroneSpotDetailPage extends StatefulWidget {
 }
 
 class _DroneSpotDetailPageState extends State<DroneSpotDetailPage> {
+  List<dynamic> _reviewTestData = [
+    {
+      'img': 'https://plus.unsplash.com/premium_photo-1675359655401-27e0b11bef70?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'name': '네릳으',
+      'place': '강릉',
+      'is_like': false,
+      'like_count': 234,
+      'content': '지방자치단체는 주민의 복리에 관한 사무를 처리하고 재산을 관리하며, 법령의 범위안에서 자치에 관한 규정을 제정할 수 있다. 헌법재판소 재판관은 정당에 가입하거나 정치에 관여할 수 없다. 예비비는 총액으로 국회의 의결을 얻어야 한다. 예비비의 지출은 차기국회의 승인을 얻어야 한다. 국가는 농지에 관하여 경자유전의 원칙이 달성될 수 있도록 노력하여야 하며, 농지의 소작제도는 금지된다.',
+      'write_date': '2024-07-05',
+      'drone': '매빅 에어2'
+    },
+    {
+      'img': 'https://plus.unsplash.com/premium_photo-1664801768830-46734d0f0848?q=80&w=1827&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'name': '아늘기',
+      'place': '신안',
+      'is_like': true,
+      'like_count': 394,
+      'content': '테스트',
+      'write_date': '2023-11-05',
+      'drone': '매빅 미니4 PRO'
+    },
+    {
+      'img': 'https://images.unsplash.com/photo-1465447142348-e9952c393450?q=80&w=1674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'name': '네릳으',
+      'place': '서울',
+      'is_like': false,
+      'like_count': 345,
+      'content': '지방자치단체는 주민의 복리에 관한 사무를 처리하고 재산을 관리하며, 법령의 범위안에서 자치에 관한 규정을 제정할 수 있다. 헌법재판소 재판관은 정당에 가입하거나 정치에 관여할 수 없다. 예비비는 총액으로 국회의 의결을 얻어야 한다. 예비비의 지출은 차기국회의 승인을 얻어야 한다. 국가는 농지에 관하여 경자유전의 원칙이 달성될 수 있도록 노력하여야 하며, 농지의 소작제도는 금지된다.',
+      'write_date': '2024-02-17',
+      'drone': '매빅 에어3'
+    },
+  ];
 
   Widget _createInfoSection() {
     return Row(
@@ -117,7 +150,7 @@ class _DroneSpotDetailPageState extends State<DroneSpotDetailPage> {
                 style: TextStyle(
                   height: 1,
                   color: Colors.black,
-                  fontSize: 18,
+                  fontSize: 20,
                   fontWeight: FontWeight.w600
                 ),
               ),
@@ -136,6 +169,26 @@ class _DroneSpotDetailPageState extends State<DroneSpotDetailPage> {
             )
           ],
         ),
+        SizedBox(height: 12,),
+        ListView.separated(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (context, idx) {
+            return ReviewFullItem(
+                img: _reviewTestData[idx]['img'],
+                name: _reviewTestData[idx]['name'],
+                place: _reviewTestData[idx]['place'],
+                content: _reviewTestData[idx]['content'],
+                likeCount: _reviewTestData[idx]['like_count'],
+                drone: _reviewTestData[idx]['drone'],
+                date: _reviewTestData[idx]['write_date']
+            );;
+          },
+          separatorBuilder: (context, idx) {
+            return SizedBox(height: 12,);
+          },
+          itemCount: _reviewTestData.length
+        )
       ],
     );
   }
@@ -158,13 +211,16 @@ class _DroneSpotDetailPageState extends State<DroneSpotDetailPage> {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        padding: EdgeInsets.fromLTRB(24, 0, 24, getBottomPaddingWithSafeHeight(context, 24)),
+        padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
         child: SingleChildScrollView(
           child: Column(
             children: [
               _createInfoSection(),
               SizedBox(height: 24,),
-              _createReviewSection()
+              _createReviewSection(),
+              SizedBox(
+                height: getBottomPaddingWithHeight(context, 24),
+              )
             ],
           ),
         ),
