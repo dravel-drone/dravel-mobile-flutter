b9bf4f430aff5f7fb9c64c2e3a31444d5c8b5ed8
K1A2 <jckim0307@gmail.com>
2024-08-10 15:54:15 +0900

    DRAV-16 design: 드론스팟 이미지 추가

--- lib/pages/detail/page_dronespot_detail.dart
+++ lib/pages/detail/page_dronespot_detail.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dravel/utils/util_ui.dart';
 import 'package:dravel/widgets/appbar/appbar_main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -8,6 +10,23 @@ class DroneSpotDetailPage extends StatefulWidget {
 }
 
 class _DroneSpotDetailPageState extends State<DroneSpotDetailPage> {
+
+  Widget _createInfoSection() {
+    return Row(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: CachedNetworkImage(
+            height: 110,
+            width: 110,
+            fit: BoxFit.cover,
+            imageUrl: 'https://images.unsplash.com/photo-1500531279542-fc8490c8ea4d?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +40,18 @@ class _DroneSpotDetailPageState extends State<DroneSpotDetailPage> {
           onPressed: () {
             Get.back();
           },
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.fromLTRB(24, 0, 24, getBottomPaddingWithSafeHeight(context, 24)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _createInfoSection()
+            ],
+          ),
         ),
       ),
     );
