61eec7a9ae4f66e6c0f785d0bd6d00f5b937a4f4
K1A2 <jckim0307@gmail.com>
2024-08-11 11:14:04 +0900

    DRAV-34: design: 리뷰 작성 페이지 이동 추가

---
+++ lib/pages/comment/page_comment_write.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CommentWritePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CommentWritePageState();
+}
+
+class _CommentWritePageState extends State<CommentWritePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
