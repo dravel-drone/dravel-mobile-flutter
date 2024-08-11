a858d18a67b26ce2efd71079a9ee965731841d09
K1A2 <jckim0307@gmail.com>
2024-08-11 16:13:23 +0900

    DRAV-41: design: 코스 텍스트 추가

--- lib/pages/detail/page_course_detail.dart
+++ lib/pages/detail/page_course_detail.dart
@@ -94,6 +94,36 @@ class _CourseDetailPageState extends State<CourseDetailPage> {
     );
   }
 
+  Widget _createCourseTextSection() {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.fromLTRB(24, 24, 24, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '2개의 경유지 / 8.2km / 4시간 30분',
+            style: TextStyle(
+              height: 1,
+              fontSize: 16,
+              fontWeight: FontWeight.w500
+            ),
+          ),
+          SizedBox(height: 12,),
+          Text(
+            '''모든 국민은 신체의 자유를 가진다. 누구든지 법률에 의하지 아니하고는 체포·구속·압수·수색 또는 심문을 받지 아니하며, 법률과 적법한 절차에 의하지 아니하고는 처벌·보안처분 또는 강제노역을 받지 아니한다. 법률은 특별한 규정이 없는 한 공포한 날로부터 20일을 경과함으로써 효력을 발생한다.
+
+모든 국민은 신속한 재판을 받을 권리를 가진다. 형사피고인은 상당한 이유가 없는 한 지체없이 공개재판을 받을 권리를 가진다. 국가는 농·어민과 중소기업의 자조조직을 육성하여야 하며, 그 자율적 활동과 발전을 보장한다. 의원을 제명하려면 국회재적의원 3분의 2 이상의 찬성이 있어야 한다. 대통령은 법률에서 구체적으로 범위를 정하여 위임받은 사항과 법률을 집행하기 위하여 필요한 사항에 관하여 대통령령을 발할 수 있다.''',
+            style: TextStyle(
+              height: 1.2,
+              color: Colors.black87,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -136,16 +166,7 @@ class _CourseDetailPageState extends State<CourseDetailPage> {
           SliverList(
             delegate: SliverChildListDelegate(
               [
-                Container(
-                  height: 2000,
-                  width: double.infinity,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 2000,
-                  width: double.infinity,
-                  color: Colors.green,
-                ),
+                _createCourseTextSection()
               ]
             )
           )
