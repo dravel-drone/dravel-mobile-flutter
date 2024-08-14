02ca2c7f959463bd1ee6b189d83ddb9732ef1704
K1A2 <jckim0307@gmail.com>
2024-08-15 00:40:20 +0900

    DRAV-39 feat: 앱 실행시 강제 로그아웃

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:dravel/controller/controller_auth.dart';
 import 'package:dravel/pages/account/page_login.dart';
 import 'package:dravel/pages/page_main_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = FlutterSecureStorage();
+  await storage.deleteAll();
+  Get.put(AuthController());
   runApp(const MyApp());
 }
 
