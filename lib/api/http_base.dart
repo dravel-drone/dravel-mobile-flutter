8a8b62e819330d8c7345df570a354f97644ee629
K1A2 <jckim0307@gmail.com>
2024-08-20 13:07:27 +0900

    DRAV-30 feat: 엑세스키 가져오는 함수 추가

--- lib/api/http_base.dart
+++ lib/api/http_base.dart
@@ -1,4 +1,11 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 class HttpBase {
+  static const FlutterSecureStorage secureStorage = FlutterSecureStorage();
   static const String baseUrl = 'https://fastapi-yolo-test.k1a2.xyz/';
   static const String domain = 'fastapi-yolo-test.k1a2.xyz';
+
+  static Future<String?> getAccessKey() async {
+    return await secureStorage.read(key: 'access');
+  }
 }
