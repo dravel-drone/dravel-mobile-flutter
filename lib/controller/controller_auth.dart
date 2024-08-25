daa1f55e88541a07848e710a8d963bff3fd10ca7
K1A2 <jckim0307@gmail.com>
2024-08-25 11:47:18 +0900

    DRAV-125 feat: 앱 재실행시 로그인 여부 체크 추가

--- lib/controller/controller_auth.dart
+++ lib/controller/controller_auth.dart
@@ -1,14 +1,24 @@
 import 'package:dravel/api/http_auth.dart';
 import 'package:dravel/model/model_auth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   static AuthController get to => Get.find();
 
+  late final FlutterSecureStorage _secureStorage;
+
   Rx<bool> isLogin = Rx(false);
 
+  @override
+  void onInit() {
+    _secureStorage = FlutterSecureStorage();
+    super.onInit();
+  }
+
   Future<bool> checkLogin() async {
-    return false;
+    return await _secureStorage.read(key: 'access') != null
+        && await _secureStorage.read(key: 'refresh') != null;
   }
 
   Future<AuthKeyModel?> login(LoginModel loginModel) async {
