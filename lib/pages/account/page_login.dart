bff560dd664da82fbd9a7de1e57af1b0b3c35f98
K1A2 <jckim0307@gmail.com>
2024-09-17 01:01:55 +0900

    feat: id/pw 찾기 임시 비활성화

--- lib/pages/account/page_login.dart
+++ lib/pages/account/page_login.dart
@@ -131,18 +131,18 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               ),
             ),
-            InkWell(
-              onTap: () {
-
-              },
-              child: Text(
-                '아이디/비밀번호 찾기',
-                style: TextStyle(
-                  color: Colors.black54,
-                  height: 1
-                ),
-              ),
-            ),
+            // InkWell(
+            //   onTap: () {
+            //
+            //   },
+            //   child: Text(
+            //     '아이디/비밀번호 찾기',
+            //     style: TextStyle(
+            //       color: Colors.black54,
+            //       height: 1
+            //     ),
+            //   ),
+            // ),
           ],
         )
       ],
