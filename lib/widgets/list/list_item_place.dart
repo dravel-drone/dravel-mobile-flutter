ff076bacd7bb99615a2ecb44f1ea62b222f4c0d0
K1A2 <jckim0307@gmail.com>
2024-09-28 02:00:57 +0900

    feat: 장소 클릭시 지도로 이동 기능 추가

--- lib/widgets/list/list_item_place.dart
+++ lib/widgets/list/list_item_place.dart
@@ -5,6 +5,7 @@ import 'package:dravel/controller/controller_location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import '../../utils/util_map.dart';
 import '../../utils/util_ui.dart';
@@ -44,134 +45,145 @@ class PlaceItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(18),
-      child: Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(14),
-        height: 110,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(18),
-          color: backgroundColor,
-        ),
-        child: Row(
-          children: [
-            if (imageUrl != null)
-              ClipRRect(
-                borderRadius: BorderRadius.circular(6),
-                child: CachedNetworkImage(
-                  height: double.infinity,
-                  width: 85,
-                  fit: BoxFit.cover,
-                  errorWidget: (context, error, obj) {
-                    return Container(
-                      width: 85,
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                              colors: getRandomGradientColor(id + 6892163)
-                          )
-                      ),
-                    );
-                  },
-                  imageUrl: imageUrl!,
-                ),
-              )
-            else
-              ClipRRect(
-                borderRadius: BorderRadius.circular(6),
-                child: Container(
-                  width: 85,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: getRandomGradientColor(id + 4353453)
-                      )
+    return GestureDetector(
+      onTap: () async {
+        // String urlString = 'kakaomap://open';
+        // if (await canLaunchUrlString(urlString)) {
+        //   launchUrlString(urlString);
+        // } else {
+        //   launchUrlString('https://map.kakao.com/link/search/$name');
+        // }
+        launchUrlString('https://map.kakao.com/link/search/제주 ${name}');
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(18),
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(14),
+          height: 110,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(18),
+            color: backgroundColor,
+          ),
+          child: Row(
+            children: [
+              if (imageUrl != null)
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: CachedNetworkImage(
+                    height: double.infinity,
+                    width: 85,
+                    fit: BoxFit.cover,
+                    errorWidget: (context, error, obj) {
+                      return Container(
+                        width: 85,
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: getRandomGradientColor(id + 6892163)
+                            )
+                        ),
+                      );
+                    },
+                    imageUrl: imageUrl!,
+                  ),
+                )
+              else
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(6),
+                  child: Container(
+                    width: 85,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: getRandomGradientColor(id + 4353453)
+                        )
+                    ),
                   ),
                 ),
-              ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(16, 8, 4, 4),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            name,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black,
-                                height: 1
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(16, 8, 4, 4),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              name,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                  height: 1
+                              ),
                             ),
                           ),
-                        ),
-                        Obx(() {
-                          if (_locationController.position.value == null) {
-                            return const SizedBox();
-                          }
-                          debugPrint(
-                            'place: ${location.lat} ${location.lng} '
-                                'my: ${_locationController.position.value!.latitude} ${_locationController.position.value!.longitude}'
-                          );
-                          return Text(
-                            formatDistance(
-                              Geolocator.distanceBetween(
-                                location.lat,
-                                location.lng,
-                                _locationController.position.value!.latitude,
-                                _locationController.position.value!.longitude
-                              ).round()
-                            ),
-                            style: TextStyle(
-                                color: Color(0xFF0075FF),
-                                fontSize: 14
-                            ),
-                          );
-                        })
-                      ],
-                    ),
-                    Text(
-                      message ?? '',
-                      style: TextStyle(
-                          color: Colors.black54,
-                          fontSize: 14,
-                          height: 1
+                          Obx(() {
+                            if (_locationController.position.value == null) {
+                              return const SizedBox();
+                            }
+                            debugPrint(
+                                'place: ${location.lat} ${location.lng} '
+                                    'my: ${_locationController.position.value!.latitude} ${_locationController.position.value!.longitude}'
+                            );
+                            return Text(
+                              formatDistance(
+                                  Geolocator.distanceBetween(
+                                      location.lat,
+                                      location.lng,
+                                      _locationController.position.value!.latitude,
+                                      _locationController.position.value!.longitude
+                                  ).round()
+                              ),
+                              style: TextStyle(
+                                  color: Color(0xFF0075FF),
+                                  fontSize: 14
+                              ),
+                            );
+                          })
+                        ],
                       ),
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.black54,
-                          size: 18,
+                      Text(
+                        message ?? '',
+                        style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 14,
+                            height: 1
                         ),
-                        SizedBox(width: 4,),
-                        Expanded(
-                          child: Text(
-                            address,
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                            softWrap: false,
-                            style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 12,
-                              height: 1
-                            ),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.black54,
+                            size: 18,
                           ),
-                        )
-                      ],
-                    ),
-                  ],
+                          SizedBox(width: 4,),
+                          Expanded(
+                            child: Text(
+                              address,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                              softWrap: false,
+                              style: TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: 12,
+                                  height: 1
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
